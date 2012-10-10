31b1801225b4275bd6d29a2f1cbc87478bbc02ca
Sadesh <nsadesh@gmail.com>
2012-10-10 23:11:56 +0800

    Bigram

--- POS/src/POSHelper.java
+++ POS/src/POSHelper.java
@@ -1,54 +1,86 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class POSHelper {
 	public HashSet<String> pennTreeBankTagSet;
+	public HashMap<String, Integer> tagCount;
+	public HashMap<String, Integer> bigramTagCount;
+	public HashMap<String, Integer> wordCount;
+	public HashMap<String, Integer> bigramWordCount;
+	public HashSet<String> breakTag; 
+	POSHelper() {
+		pennTreeBankTagSet = new HashSet<String>();
+		tagCount = new HashMap<String, Integer>();
+		wordCount = new HashMap<String, Integer>();
+		bigramWordCount=new HashMap<String, Integer>();
+		bigramTagCount=new HashMap<String, Integer>();
+		breakTag=new HashSet<String>();
+		generatePennTreeBankTagSet();
+		intializeHashMap();
+	}
+
+	private void intializeHashMap() {
+		Iterator<String> itr = pennTreeBankTagSet.iterator();
+		while (itr.hasNext()) {
+			tagCount.put(itr.next(), 0);
+		}
+
+	}
 
 	public void generatePennTreeBankTagSet() {
-	pennTreeBankTagSet=new HashSet<String>();
-	pennTreeBankTagSet.add("CC"); 
-	pennTreeBankTagSet.add("CD"); 
-	pennTreeBankTagSet.add("DT");
-	pennTreeBankTagSet.add("EX");
-	pennTreeBankTagSet.add("FW");
-	pennTreeBankTagSet.add("IN"); 
-	pennTreeBankTagSet.add("JJ"); 
-	pennTreeBankTagSet.add("JJR"); 
-	pennTreeBankTagSet.add("JJS"); 
-	pennTreeBankTagSet.add("LS"); 
-	pennTreeBankTagSet.add("MD"); 
-	pennTreeBankTagSet.add("NN"); 
-	pennTreeBankTagSet.add("NNS"); 
-	pennTreeBankTagSet.add("NNP"); 
-	pennTreeBankTagSet.add("NNPS"); 
-	pennTreeBankTagSet.add("PDT"); 
-	pennTreeBankTagSet.add("POS"); 
-	pennTreeBankTagSet.add("PRP"); 
-	pennTreeBankTagSet.add("PRP$"); 
-	pennTreeBankTagSet.add("RB"); 
-	pennTreeBankTagSet.add("RBR"); 
-	pennTreeBankTagSet.add("RBS"); 
-	pennTreeBankTagSet.add("RP"); 
-	pennTreeBankTagSet.add("SYM");
-	pennTreeBankTagSet.add("TO"); 
-	pennTreeBankTagSet.add("UH"); 
-	pennTreeBankTagSet.add("VB"); 
-	pennTreeBankTagSet.add("VBD"); 
-	pennTreeBankTagSet.add("VBG"); 
-	pennTreeBankTagSet.add("VBN"); 
-	pennTreeBankTagSet.add("VBP"); 
-	pennTreeBankTagSet.add("VBZ"); 
-	pennTreeBankTagSet.add("WDT"); 
-	pennTreeBankTagSet.add("WP"); 
-	pennTreeBankTagSet.add("WP$"); 
-	pennTreeBankTagSet.add("WRB"); 
-	pennTreeBankTagSet.add("#");
-	pennTreeBankTagSet.add("$");
-	pennTreeBankTagSet.add("'");
-	pennTreeBankTagSet.add("(");
-	pennTreeBankTagSet.add(")");
-	pennTreeBankTagSet.add(",");
-	pennTreeBankTagSet.add(".");
-	pennTreeBankTagSet.add(":");
-	pennTreeBankTagSet.add("\""); 	
-}
+
+		pennTreeBankTagSet.add("CC");
+		pennTreeBankTagSet.add("CD");
+		pennTreeBankTagSet.add("DT");
+		pennTreeBankTagSet.add("EX");
+		pennTreeBankTagSet.add("FW");
+		pennTreeBankTagSet.add("IN");
+		pennTreeBankTagSet.add("JJ");
+		pennTreeBankTagSet.add("JJR");
+		pennTreeBankTagSet.add("JJS");
+		pennTreeBankTagSet.add("LS");
+		pennTreeBankTagSet.add("MD");
+		pennTreeBankTagSet.add("NN");
+		pennTreeBankTagSet.add("NNS");
+		pennTreeBankTagSet.add("NNP");
+		pennTreeBankTagSet.add("NNPS");
+		pennTreeBankTagSet.add("PDT");
+		pennTreeBankTagSet.add("POS");
+		pennTreeBankTagSet.add("PRP");
+		pennTreeBankTagSet.add("PRP$");
+		pennTreeBankTagSet.add("RB");
+		pennTreeBankTagSet.add("RBR");
+		pennTreeBankTagSet.add("RBS");
+		pennTreeBankTagSet.add("RP");
+		pennTreeBankTagSet.add("SYM");
+		pennTreeBankTagSet.add("TO");
+		pennTreeBankTagSet.add("UH");
+		pennTreeBankTagSet.add("VB");
+		pennTreeBankTagSet.add("VBD");
+		pennTreeBankTagSet.add("VBG");
+		pennTreeBankTagSet.add("VBN");
+		pennTreeBankTagSet.add("VBP");
+		pennTreeBankTagSet.add("VBZ");
+		pennTreeBankTagSet.add("WDT");
+		pennTreeBankTagSet.add("WP");
+		pennTreeBankTagSet.add("WP$");
+		pennTreeBankTagSet.add("WRB");
+		pennTreeBankTagSet.add("#");
+		pennTreeBankTagSet.add("$");
+		pennTreeBankTagSet.add("``");
+		pennTreeBankTagSet.add("-LRB-");
+		pennTreeBankTagSet.add("-RRB-");
+		pennTreeBankTagSet.add(",");
+		pennTreeBankTagSet.add(".");
+		pennTreeBankTagSet.add(":");
+		pennTreeBankTagSet.add("\'\'");
+		breakTag.add("#");
+		breakTag.add("$");
+		breakTag.add("``");
+		breakTag.add(",");
+		breakTag.add(".");
+		breakTag.add("\'\'");
+		breakTag.add(":");
+	}
 }
--- POS/src/build_tagger.java
+++ POS/src/build_tagger.java
@@ -1,55 +1,160 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class build_tagger {
+	static POSHelper ph;
+
 	public static void main(String args[]) {
 		File file = new File("/home/user/NLP/a2_data/sents.train");
 		FileReader read;
 		BufferedReader buffRead;
+		ph = new POSHelper();
 		try {
 			read = new FileReader(file);
-			buffRead=new BufferedReader(read);
+			buffRead = new BufferedReader(read);
 			String temp;
-			int i=0;
-			int j=0;
-			int k=0;
-			/*while(null!=(temp=buffRead.readLine()))
-				i++;*/
-	
-			
-			while(null!=(temp=buffRead.readLine())){
-				StringTokenizer token=new StringTokenizer(temp," ");
-				i++;
-			while(token.hasMoreTokens())
-			{j++;
-				String temp1=token.nextToken();
-			
-				StringTokenizer anothertoken=new StringTokenizer(temp1,"/");
-				while(anothertoken.hasMoreTokens())
-				{
-					String testToken=anothertoken.nextToken();
-					if(testToken.equals("VBZ"))
-					k++;	
-				//System.out.println(testToken);
+			int i = 0;
+			int j = 0;
+			int k = 0;
+			String bigramTags, wordTagPair;
+			boolean flag = true;
+			List<String> li = new ArrayList<String>();
+			String tag, word,prevTag;
+			int count = 0;
+			List<String> sentences = new ArrayList<String>();
+			while (null != (temp = buffRead.readLine())) {
+				if (temp.contains("/.")) {
+					while (temp.indexOf("/.") > 0) {
+						sentences.add("<s> "
+								+ temp.substring(0, temp.indexOf("/.") + 2)
+								+ " <\\s>");
+						temp = temp.substring(temp.indexOf("/.") + 2);
+					}
+				} else {
+					sentences.add("<s> " + temp + " <\\s>");
 				}
-			}}
-			System.out.println(j);
-			System.out.println(i);
-			System.out.println(k);
+			}
+
+			Iterator<String> sentenceIterator = sentences.iterator();
+			while (sentenceIterator.hasNext()) {
+				i++;
+				temp=sentenceIterator.next();
+				//System.out.println(temp);
+				StringTokenizer token = new StringTokenizer(
+						temp, " ");
+				prevTag = token.nextToken();
+				wordTagPair = null;
+				while (token.hasMoreTokens()) {
+					StringTokenizer bigramToken = new StringTokenizer(
+							token.nextToken(), "/");
+					
+					if (bigramToken.countTokens() >= 2) {
+						List<String> words = new ArrayList<String>();
+						for (int l = 1; l < bigramToken.countTokens(); l++)
+							words.add(bigramToken.nextToken());
+						tag = bigramToken.nextToken();
+						addListofWordTags(words, tag);
+						if (ph.pennTreeBankTagSet.contains(tag)) {
+						  ph.tagCount.put(tag, ph.tagCount.get(tag) + 1);}
+						bigramTags=prevTag+"|"+tag;
+						addBigramTagTag(bigramTags);
+						prevTag=tag;
+					}
+					else if(bigramToken.countTokens() == 1)
+					{
+						tag = bigramToken.nextToken();
+						bigramTags=prevTag+"|"+tag;
+						addBigramTagTag(bigramTags);
+						prevTag=tag;
+					}
+				}
+			
+			}
+			/*
+			 * StringTokenizer token = new StringTokenizer(temp, "./.");
+			 * System.out.println(temp); i++; if(i>3) break; while
+			 * (token.hasMoreTokens()) { j++; String temp1 = token.nextToken();
+			 * 
+			 * StringTokenizer anothertoken = new StringTokenizer(temp1, "/");
+			 * flag = true;
+			 * 
+			 * while (anothertoken.hasMoreTokens()) { String testToken =
+			 * anothertoken.nextToken().trim(); if
+			 * (ph.pennTreeBankTagSet.contains(testToken)) {
+			 * ph.tagCount.put(testToken, ph.tagCount.get(testToken) + 1); flag
+			 * = false; } else { if (ph.wordCount.containsKey(testToken)) {
+			 * ph.wordCount.put(testToken, ph.wordCount.get(testToken) + 1); }
+			 * else { ph.wordCount.put(testToken, 1); } } if
+			 * (ph.breakTag.contains(testToken)) { li.add(testToken); if
+			 * (ph.wordCount.containsKey(testToken)) {
+			 * ph.wordCount.put(testToken, ph.wordCount.get(testToken) + 1); }
+			 * else { ph.wordCount.put(testToken, 1); } break; } }
+			 * 
+			 * }
+			 * 
+			 * /* System.out.println(ph.tagCount); System.out.println(i);
+			 * Iterator<String> itr = ph.tagCount.keySet().iterator(); k = 0;
+			 * while (itr.hasNext()) k = k + ph.tagCount.get(itr.next());
+			 * 
+			 * System.out.println(k);
+			 * System.out.println(ph.wordCount.keySet().size()); int h = 0;
+			 * Iterator<String> itr1 = ph.wordCount.keySet().iterator(); while
+			 * (itr1.hasNext()) h = h + ph.wordCount.get(itr1.next());
+			 * System.out.println("h=" + h); File fe = new
+			 * File("WordCount.txt"); FileWriter fw = new FileWriter(fe);
+			 * BufferedWriter bw = new BufferedWriter(fw);
+			 * bw.write(ph.wordCount.toString()); bw.close();
+			 * System.out.println(count);
+			 */
+			System.out.println(ph.bigramWordCount.size());
+			System.out.println(ph.tagCount.size());
+			System.out.println(ph.bigramTagCount.size());
+			System.out.println(ph.bigramTagCount.get("<s>|IN"));
+
 		} catch (FileNotFoundException e) {
 			System.out
 					.println("The file path is wrong for training file so unable to complete the model");
 			System.exit(0);
 
-		}
-		 catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	private static void addListofWordTags(List<String> words, String tag) {
+		Iterator<String> wordItr = words.iterator();
+		while (wordItr.hasNext()) {
+			String word = wordItr.next();
+			String wordTagPair = word + "|" + tag;
+			addBigramWordTags(wordTagPair);
+		}
+
+	}
+
+	static void addBigramWordTags(String wordTagPair) {
+		if (ph.bigramWordCount.containsKey(wordTagPair)) {
+			ph.bigramWordCount.put(wordTagPair,
+					ph.bigramWordCount.get(wordTagPair) + 1);
+		} else {
+			ph.bigramWordCount.put(wordTagPair, 1);
+		}
+	}
+	static void addBigramTagTag(String bigramTags) {
+		if (ph.bigramTagCount.containsKey(bigramTags)) {
+			ph.bigramTagCount.put(bigramTags,
+					ph.bigramTagCount.get(bigramTags) + 1);
+		} else {
+			ph.bigramTagCount.put(bigramTags, 1);
+		}
 	}
 }
